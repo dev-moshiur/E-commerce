8694f4dc06175d8b4d8a1180b302019c8334ea5d
MD: Moshiur Rahman <115656227+dev-moshiur@users.noreply.github.com>
2023-02-06 00:04:53 +0600

    Update Topbar.jsx
    commited

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -84,7 +84,7 @@ export default function Topbar() {
                 <Link to='/contact'>Contact</Link>
             </div>
             <div className={searchOpen ? "search show":'search'}>
-                <div className="main" onClick={()=>{if(searchOpen){setSearchOpen((pre)=>!pre)}}}>
+                <div className="main" onClick={()=>setSearchOpen((pre)=>!pre)}>
                     <Search/>
                 </div>
                 <div className="toggle">
